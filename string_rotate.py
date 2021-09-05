9623c7b2e2cab71617fd1748672b1ca603de7a60
AshutoshMaleti <ashutosh.maleti@dianapps.com>
2021-09-05 15:41:50 +0530

    modified string_rotate problem

--- string_rotate.py
+++ string_rotate.py
@@ -2,10 +2,8 @@ string=input()
 rotateNum=int(input())
 
 def rotate_string(string,rotateNum):
-    output_string=[None for _ in range(len(string))]
-    for rotation in range(len(string)):
-        output_string[rotation-rotateNum]=string[rotation]
+    length=len(string)
+    print('left rotate: '+string[rotateNum:]+string[:rotateNum])
+    print('right rotate: '+string[length-rotateNum:]+string[:length-rotateNum])
 
-    return ''.join(output_string)
-
-print(rotate_string(string,rotateNum))
+rotate_string(string,rotateNum)
